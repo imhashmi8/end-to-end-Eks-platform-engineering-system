a872926cfee430e8124c8eafd963b8324a62bf1f
imhashmi8 <qamar.hashmi@outlook.com>
2026-03-28 11:27:48 +0530

    changed the bucket name for state files

--- terraform/envs/dev/backend.tf
+++ terraform/envs/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "qamar-terraform-state"
+    bucket       = "qamar-terraform-state-ap-south-1"
     key          = "eks/dev/terraform.tfstate"
     region       = "ap-south-1"
     use_lockfile = true
--- terraform/envs/prod/backend.tf
+++ terraform/envs/prod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "qamar-terraform-state"
+    bucket       = "qamar-terraform-state-ap-south-1"
     key          = "eks/prod/terraform.tfstate"
     region       = "ap-south-1"
     use_lockfile = true
