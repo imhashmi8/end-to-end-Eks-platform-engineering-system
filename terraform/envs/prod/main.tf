53c9b78d66a7f488e52a2dc473a72c893251a101
imhashmi8 <qamar.hashmi@outlook.com>
2026-03-28 10:58:43 +0530

    added prod env terraform template

---
+++ terraform/envs/prod/main.tf
@@ -0,0 +1,23 @@
+module "vpc" {
+  source = "../../modules/vpc"
+
+  name = "prod-vpc"
+  cidr = "10.20.0.0/16"
+
+  azs             = ["ap-south-1a", "ap-south-1b"]
+  private_subnets = ["10.20.1.0/24", "10.20.2.0/24"]
+  public_subnets  = ["10.20.101.0/24", "10.20.102.0/24"]
+}
+
+module "eks" {
+  source = "../../modules/eks"
+
+  cluster_name    = "prod-eks"
+  vpc_id          = module.vpc.vpc_id
+  private_subnets = module.vpc.private_subnets
+
+  instance_types = ["t3.large"]
+  min_size       = 2
+  max_size       = 5
+  desired_size   = 3
+}
