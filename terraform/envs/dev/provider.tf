87039201ed8bb6fdb1a187dc38453dfdec42292f
imhashmi8 <qamar.hashmi@outlook.com>
2026-03-28 18:06:21 +0530

    Replace Terraform provider symlinks with regular files

--- terraform/envs/dev/provider.tf
+++ terraform/envs/dev/provider.tf
@@ -1,1 +1,13 @@
-../../global/provider.tf
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = var.region
+  profile = var.aws_profile
+}
