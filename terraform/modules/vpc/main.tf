7d4e8ecc1bd0dce4dba68882a8b73581a3347732
imhashmi8 <qamar.hashmi@outlook.com>
2026-03-29 14:40:39 +0530

    added the aws loadbalancer controlled to expose application

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -9,6 +9,15 @@ module "vpc" {
   private_subnets = var.private_subnets
   public_subnets  = var.public_subnets
 
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+
+
   enable_nat_gateway = true
   single_nat_gateway = true
 }
